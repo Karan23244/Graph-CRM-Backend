2ca3b77834ebc6b907abc95675e3ce6ef7d85865
Karan23244 <karan@clickorbits.com>
2025-08-25 18:34:18 +0530

    perivious changes

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ const pool = require("./config/db");
 dotenv.config();
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+//app.use(cors());
+//app.use(express.json());
+
+//  ^|^e CORS FIRST
+
+//  ^|^e Increase body size limit
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ limit: "500mb", extended: true }));
+
+//  ^|^e Routes
 app.use("/api", uploadRoutes);
 app.use("/api", campaignRoutes);
 
